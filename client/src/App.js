e6bf9fe28da1c0e265365d89ce375d94b8da14d1
urxt <80124020+urxt@users.noreply.github.com>
2023-01-28 16:08:17 -0500

    switched out leaflet for ol

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import React, { Component } from 'react';
 import render from 'react-dom';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+
+const user = {
+  name: 'John Jones',
+  email: "john@jones.com",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
+  imageSize: 90,
+};
+
+const MapContext = new React.createContext();
 
 export default function App(){
     /*
@@ -15,6 +22,7 @@ export default function App(){
       <h1>MeetYU</h1>
       <MapButton />
       <GenerateMap />
+      <User />
     </div>
    );
 }
@@ -29,28 +37,9 @@ function GenerateMap() {
   return ( 
     <>
       <h3>York University Map (Keele Campus)</h3>
-
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
     </>
   );
 }
-
-const user = {
-  name: 'John Jones',
-  email: "john@jones.com",
-  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
-  imageSize: 90,
-};
 
 function User() {
   return(
