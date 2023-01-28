4d449d0b8062fcfd303ed2936a19acdc6ee32c5d
urxt <80124020+urxt@users.noreply.github.com>
2023-01-28 14:08:13 -0500

    git map to post

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import React, { Component } from 'react';
+import { Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 function App(){
   return (
-    <div>
-      <Welcome name="Daniel" />
-    </div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
