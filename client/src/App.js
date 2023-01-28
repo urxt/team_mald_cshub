51bc6098bb33ff01fb74ed136e62381f5e8c43d4
urxt <80124020+urxt@users.noreply.github.com>
2023-01-28 15:01:15 -0500

    changed App.js to have several functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,47 @@
 import './App.css';
 import React, { Component } from 'react';
-import { Marker, Popup } from 'react-leaflet';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { render } from 'react-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App(){
   return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div>
+      <h1>MeetYU</h1>
+      <MapButton />
+      <User />
+    </div>
   );
 }
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function MapButton() {
+  return (
+    <button>Generate Map</button>
+  );
 }
 
-function User (name, age, gender, email) {
-  this.age = age;
-  this.gender = gender;
-  this.email = email;
-  return this.name;
+const user = {
+  name: 'John Jones',
+  email: "john@jones.com",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
+  imageSize: 90,
+};
+
+function User() {
+  return(
+    <>
+      <h4>Name: {user.name}</h4>
+      <h4>Email: {user.email}</h4>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
 }
 
 User.prototype.toString = function UserToString() {
