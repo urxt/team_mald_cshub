b4a52fc9215cd4dca032eb2ccf9e42733acba3eb
Mouiz Ahmed <mouizahmed1@gmail.com>
2023-01-28 17:01:41 -0500

    fixed routes merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import * as ReactDOM from 'react-dom';
-<<<<<<< HEAD
 
 import { Marker, Popup } from 'react-leaflet';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
@@ -23,35 +21,9 @@ function App(){
         </Routes>
       </Router>
 
-=======
-import EventPage from './Pages/EventPage';
-import EventSidebar from './Components/EventSidebar';
-import CreateEvent from './Pages/CreateEvent';
-
-const user = {
-  name: 'John Jones',
-  email: "john@jones.com",
-  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
-  imageSize: 90,
-};
-
-export default function App(){
-    /*
-    <div className="App">
-      <EventSidebar className="sidebar"></EventSidebar>
-      <EventPage></EventPage>
->>>>>>> 9f1b80111b2daa4093b26f5918d70a395023494d
       {/* <CreateEvent></CreateEvent> */}
     </div>
-    */
-   return (
-    <div>
-      <h1>MeetYU</h1>
-      <MapButton />
-      <GenerateMap />
-      <User />
-    </div>
-   );
+  );
 }
 
 function MapButton() {
@@ -60,13 +32,12 @@ function MapButton() {
   );
 }
 
-function GenerateMap() {
-  return ( 
-    <>
-      <h3>York University Map (Keele Campus)</h3>
-    </>
-  );
-}
+const user = {
+  name: 'John Jones',
+  email: "john@jones.com",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
+  imageSize: 90,
+};
 
 function User() {
   return(
@@ -85,3 +56,9 @@ function User() {
     </>
   );
 }
+
+User.prototype.toString = function UserToString() {
+  return `Name: ${this.name}\n Age: ${this.age}\n Email: ${this.email}\n Gender: ${this.gender}`;
+}
+
+export default App;
