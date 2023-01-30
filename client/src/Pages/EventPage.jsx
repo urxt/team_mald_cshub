572eb5c1911f59793c7627b64be75bb4b1837582
urxt <80124020+urxt@users.noreply.github.com>
2023-01-30 13:15:43 -0500

    event page changes

--- client/src/Pages/EventPage.jsx
+++ client/src/Pages/EventPage.jsx
@@ -4,13 +4,29 @@ import Event from "../Components/Event"
 function EventPage() {
     return(
         <div className="eventPage">
+
             <div className="details">
                 <Event pageCaller="eventPage"></Event>
             </div>
-            <div className="map">
+
+            <div className="Event A">
+                <Event pageCaller="eventPage"></Event>
             </div>
+
+            <div className="Event B">
+                <Event pageCaller="eventPage"></Event>
+            </div>
+
+            <div className="Event C">
+                <Event pageCaller="eventPage"></Event>
+            </div>
+
+            <div className="map">
+                <Event pageCaller="eventPage"></Event>
+            </div>
+
         </div>
     )
 }
 
-export default EventPage
+export default EventPage;
